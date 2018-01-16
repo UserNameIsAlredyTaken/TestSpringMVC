a0484477dc69f8a75087e38ea578107b9c24d9fd
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2018-01-17 01:49:23 +0300

    Написал нормальный контроллер

--- src/ru/battlefield/my/Application.java
+++ src/ru/battlefield/my/Application.java
@@ -9,44 +9,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.battlefield.my.repository.CamosRepository;
 
-@Slf4j
+//@Slf4j
 @SpringBootApplication
-@ComponentScan("ru.battlefield.my.domain")
+//@ComponentScan("ru.battlefield.my.domain")
 public class Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class,args);
     }
 
 
-    @Bean
-    public CommandLineRunner demo(CamosRepository repository){
-        return (args)-> {
-
-            log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:");
-            log.info("--------------------------------------------");
-            for (Camo cam : repository.findAllByNecessaryLvlIsLessThanEqual(15)){
-                log.info(cam.toString());
-            }
-            /*for (Specialization se : repository.findByNecessaryLvlIsLessThanEqual(41)){
-                log.info(se.toString());
-            }*/
-            /*for (Gadget ge : repository.findAllByNecessaryPointIsLessThanEqual(29000)){
-                log.info(ge.toString());
-            }*/
-            /*for (Weapon we : repository.findAllByMinDamageIsGreaterThanEqual(20.0)){
-                log.info(we.toString());
-            }*/
-            /*for (CommonWeapon cw : repository.findAllByLvlForUnlockIsLessThanEqual(15)){
-                log.info(cw.toString());
-            }*/
-            /*for (ClassWeapon clw : repository.findAllByPointsForUnlockIsLessThanEqual(110000)){
-                log.info(clw.toString());
-            }*/
-            /*for (Upgrade u : repository.findAllByKillsForUnlockIsLessThanEqual(15)){
-                log.info(u.toString());
-            }*/
-            log.info("");
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(CamosRepository repository){
+//        return (args)-> {
+//
+//            log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:");
+//            log.info("--------------------------------------------");
+//            for (Camo cam : repository.findAllByNecessaryLvlIsLessThanEqual(15)){
+//                log.info(cam.toString());
+//            }
+//            /*for (Specialization se : repository.findByNecessaryLvlIsLessThanEqual(41)){
+//                log.info(se.toString());
+//            }*/
+//            /*for (Gadget ge : repository.findAllByNecessaryPointIsLessThanEqual(29000)){
+//                log.info(ge.toString());
+//            }*/
+//            /*for (Weapon we : repository.findAllByMinDamageIsGreaterThanEqual(20.0)){
+//                log.info(we.toString());
+//            }*/
+//            /*for (CommonWeapon cw : repository.findAllByLvlForUnlockIsLessThanEqual(15)){
+//                log.info(cw.toString());
+//            }*/
+//            /*for (ClassWeapon clw : repository.findAllByPointsForUnlockIsLessThanEqual(110000)){
+//                log.info(clw.toString());
+//            }*/
+//            /*for (Upgrade u : repository.findAllByKillsForUnlockIsLessThanEqual(15)){
+//                log.info(u.toString());
+//            }*/
+//            log.info("");
+//        };
+//    }
 }
--- src/ru/battlefield/my/controller/MainController.java
+++ src/ru/battlefield/my/controller/MainController.java
@@ -1,33 +1,31 @@
 package ru.battlefield.my.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ru.battlefield.my.domain.Camo;
-import ru.battlefield.my.service.CamoService;
+import org.springframework.web.bind.annotation.RestController;
+import ru.battlefield.my.domain.PlayerProfile;
+import ru.battlefield.my.repository.PlayerProfilesRepository;
+
+import java.util.List;
 
 /**
- * Created by danil on 09.01.2018.
+ * Created by danil on 16.01.2018.
  */
-
-@Controller
+@RestController
 public class MainController {
-    private CamoService camoService;
-    @Autowired(required = true)
-    public void setCamoService(CamoService camoService) {
-        this.camoService = camoService;
+
+    @Autowired
+    private PlayerProfilesRepository playerProfilesRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/getAllUsers")
+    public List<PlayerProfile> getAllUsers(){
+        System.out.println("Xnjnj");
+        return (List<PlayerProfile>) playerProfilesRepository.findAll();
     }
 
-    @RequestMapping(value = "camos", method = RequestMethod.GET)
-    public String listCamos(Model model){
-        model.addAttribute("listCamos", this.camoService.findAll());
-        model.addAttribute("camo", new Camo());
-        return "camos";
+    @RequestMapping(method = RequestMethod.GET, value = "/getString")
+    public String getString(){
+        return "String";
     }
-
-
-
 }
